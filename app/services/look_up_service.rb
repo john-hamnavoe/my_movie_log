91c2b53277ec939a9b99822dc89bbb9a7b153941
John H Wallace <johnhwallace74@gmail.com>
2018-02-20 08:08:17 +0000

    change lookup service to module

--- app/services/look_up_service.rb
+++ app/services/look_up_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class LookUpService
+module LookUpService
   def self.lookup_values(model, options = { id_to_include: nil })
     model_class = model_class(model)
     model_class.all.order(:name) if model_class

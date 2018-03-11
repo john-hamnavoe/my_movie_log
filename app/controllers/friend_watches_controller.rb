77b977d0d385a945d7a248bd4c8cb93e98d885fd
John H Wallace <johnhwallace74@gmail.com>
2018-03-11 09:05:42 +0000

    put basic friend watches controller test

--- app/controllers/friend_watches_controller.rb
+++ app/controllers/friend_watches_controller.rb
@@ -1,4 +1,6 @@
 class FriendWatchesController < ApplicationController
+  before_action :logged_in_user
+
   def index
     @watches = Watch.eager_load(user: :friendships).where('friendships.friend_id = ?', 
       current_user.id).details.paginate(page: params[:page])
--- test/controllers/friend_watches_controller_test.rb
+++ test/controllers/friend_watches_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class FriendWatchesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get friend_watches_index_url
-    assert_response :success
+    get friend_feed_path
+    assert_redirected_to login_path
+    log_in_as(users(:michael))
+    get friend_feed_path
+    assert :success
   end
 
 end
